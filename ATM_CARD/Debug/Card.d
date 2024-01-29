1a377059fa77a1fe13fadd0f8b43dc819b258d6b
Youssef Hesham ElSayyad <Youssef_elsayad3@gmail.com>
2024-01-29 22:55:41 +0200

    IBAN Recieving and Sending

---
+++ ATM_CARD/Debug/Card.d
@@ -0,0 +1,14 @@
+Card.o Card.o: ../Card.c ../Card.h ../spi.h ../std_types.h ../usart.h \
+  ../common_macros.h ../lcd.h
+
+../Card.h:
+
+../spi.h:
+
+../std_types.h:
+
+../usart.h:
+
+../common_macros.h:
+
+../lcd.h:
---
+++ ATM_Machine/Debug/ATM.d
@@ -0,0 +1,12 @@
+ATM.o ATM.o: ../ATM.c ../ATM.h ../spi.h ../std_types.h ../lcd.h \
+  ../common_macros.h
+
+../ATM.h:
+
+../spi.h:
+
+../std_types.h:
+
+../lcd.h:
+
+../common_macros.h:
